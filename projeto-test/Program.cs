4088bba3138b227fa15d0ff5cb1f0537beb5ec8f
Bruno César <bruno.vicente@aluno.ufop.edu.br>
2021-01-19 14:52:18 -0300

    Projeto iniciado,mostrando a msg ola mundo

---
+++ projeto-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projeto_test {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá seus cuzoes!");
+
+            int number = int.Parse(Console.ReadLine());
+        }
+    }
+}
